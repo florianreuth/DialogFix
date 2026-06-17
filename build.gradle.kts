332a2d583f0901c46f9bee87f6132829c9e040ae
Florian Reuth <git@florianreuth.de>
2026-06-17 23:53:32 +0200

    DialogFix 1.1.3: Minecraft 26.2
    Signed-off-by: Florian Reuth <git@florianreuth.de>

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,11 +8,3 @@ plugins {
 setupProject()
 setupFabric()
 setupPublishing()
-
-repositories {
-    maven("https://maven.terraformersmc.com/releases")
-}
-
-dependencies {
-    compileOnly("com.terraformersmc:modmenu:18.0.0-alpha.8")
-}
